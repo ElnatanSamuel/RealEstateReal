f650f0d9a58cee5668c9ef7926debadf3b5a9f3a
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2024-04-27 16:38:42 +0300

    searh modal

--- app/(screens)/(tabs)/_layout.js
+++ app/(screens)/(tabs)/_layout.js
@@ -9,56 +9,56 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import Featured from "./featured"
-import Results from "./results"
+import index from "./index"  
 import Reccomendation from "./reccomendation"
 // const Stack = createStackNavigator();
 
 const Stack = createMaterialTopTabNavigator();
 
 // native
-export default function TabLayout(){
-    <NavigationContainer independent={false} >
-    <Stack.Navigator >
-      <Stack.Screen name="Result" component={Results} />
-      <Stack.Screen name="Featured" component={Featured} />
-      <Stack.Screen name="Reccomendation" component={Reccomendation} />
-    </Stack.Navigator>
-  </NavigationContainer>
-}
+// export default function TabLayout(){
+//     <NavigationContainer independent={false} >
+//     <Stack.Navigator >
+//       <Stack.Screen name="Result" component={Results} />
+//       <Stack.Screen name="Featured" component={Featured} />
+//       <Stack.Screen name="Reccomendation" component={Reccomendation} />
+//     </Stack.Navigator>
+//   </NavigationContainer>
+// }
 
 // expo
-// export default function TabLayout() {
-//   return (
-//     <Tabs 
-//     initialRouteName='results'
-//     screenOptions={{ tabBarActiveTintColor: 'blue' }}>
-//       <Tabs.Screen
-//         name="results"
-//         options={{
-//         title: 'Results',
-//         // href:null,
-//         headerShown: false, 
-//         tabBarIcon: ({ color }) => <FontAwesome size={28} name="list" color={color} />,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="reccomendation"
-//         options={{
-//           // title: 'Reccomendation',
-//         //   href:null,
-//           headerShown: false,
-//           // tabBarLabel: 'Profile'
-//           // tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
-//         }} />
-//         <Tabs.Screen
-//         name="featured"
-//         options={{
-//           title: 'featured',
-//         //   href:null,
-//           headerShown: false,
-//           tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
-//         }}
-//       />
-//     </Tabs>
-//   );
-// }
+export default function TabLayout() {
+  return (
+    <Tabs 
+    // initialRouteName='results'
+    screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+        title: 'Results',
+        // href:null,
+        headerShown: false, 
+        tabBarIcon: ({ color }) => <FontAwesome size={28} name="list" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="reccomendation"
+        options={{
+          // title: 'Reccomendation',
+        //   href:null,
+          headerShown: false,
+          // tabBarLabel: 'Profile'
+          // tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }} />
+        <Tabs.Screen
+        name="featured"
+        options={{
+          title: 'featured',
+        //   href:null,
+          headerShown: false,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
